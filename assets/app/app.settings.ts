ceb8718f8a00e8112b506d25041f1d922089ee62
Steve McKinnon <mckinn_git@yahoo.com>
2017-05-06 23:46:09 -0400

    setting up the environment again

--- assets/app/app.settings.ts
+++ assets/app/app.settings.ts
@@ -1,4 +1,4 @@
 export class AppSettings {
-   public static API_ENDPOINT='http://localhost:3000/';
-   // public static API_ENDPOINT='https://blucore.herokuapp.com/';
+   /// public static API_ENDPOINT='http://localhost:3000/';
+   public static API_ENDPOINT='https://blucore.herokuapp.com/';
 }
